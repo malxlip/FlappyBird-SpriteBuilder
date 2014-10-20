e36ccc017e58ed18c37d691f0a96fefab19e3267
malxlip <malcolmlipscomb@Agent-14.local>
2014-10-20 16:18:57 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,6 +9,8 @@
     // your code here
     character = (Character *)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
+    timeSinceObstacle = 0.0f;
+    
     [self addObstacle];
     
 }
@@ -16,6 +18,13 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    
+    if (timeSinceObstacle > 2.0f) {
+        [self addObstacle];
+        timeSinceObstacle = 0.0f;
+        
+    }
 }
 
 // put new methods here
